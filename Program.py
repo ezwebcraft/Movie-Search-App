2e8aa3f8bfbe8e6b2db5c6034d7d1d15ce25d4ce
Elvin Ramirez <real8686@gmail.com>
2018-11-10 21:18:27 -0500

    quick add search for hits

--- Program.py
+++ Program.py
@@ -1,9 +1,18 @@
-
-
 import requests
+import collections
 
-search = 'capital'
 
-url = 'http://movie_service.talkpython.fm/api/search/capital'.format(search)
+search = "capital"
 
-print(url)
+url = "http://movie_service.talkpython.fm/api/search/capital".format(search)
+
+resp = requests.get(url)
+
+resp.raise_for_status()
+
+movie_data = resp.json()
+
+movies = movie_data.get('hits')   
+#print(resp.status_code)
+
+print(type(movies),": \n",movies)
