588e7e6e5ac8f8852ccfb4369e9604df5f49f905
Elvin Ramirez <real8686@gmail.com>
2018-11-12 10:39:40 -0500

    test func for movies

--- Program.py
+++ Program.py
@@ -16,6 +16,28 @@ resp.raise_for_status()
 movie_data = resp.json()
 movies_list = movie_data.get("hits")
 
+# movies = []
+# for md in movies_list:
+#     m = MovieResult(
+#         imdb_code=md.get("imdb_code"),
+#         title=md.get("title"),
+#         duration=md.get("duration"),
+#         director=md.get("director"),
+#         year=md.get("year", 0),
+#         rating=md.get("rating", 0),
+#         imdb_score=md.get("imdb_score", 0.0),
+#         keywords=md.get("keywords"),
+#         genres=md.get("genres")
+#     )
+#     movies.append(m)
+
+def method(x,y,z):
+    pass
+
+
+def method(7,2,z=2)
+
+
 movies = []
 for md in movies_list:
     m = MovieResult(
@@ -29,8 +51,7 @@ for md in movies_list:
         keywords=md.get("keywords"),
         genres=md.get("genres")
     )
-    movies.append(m)
-
+    movies.append(m) 
 
 print("Found {} movies from search {}".format(len(movies), search))
 
