debb4e40962e6dd856e77f9e210408b6e9d8df20
Elvin Ramirez <real8686@gmail.com>
2018-11-12 21:03:04 -0500

    clean and format

--- Program.py
+++ Program.py
@@ -6,8 +6,6 @@ MovieResult = collections.namedtuple(
     "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres",
 )
 
-# search = 'runner'
-
 search = input("Enter a name of a movie: ")
 
 url = "http://movie_service.talkpython.fm/api/search/{}".format(search)
@@ -18,35 +16,8 @@ resp.raise_for_status()
 movie_data = resp.json()
 movies_list = movie_data.get("hits")
 
-# movies = []
-# for md in movies_list:
-#     m = MovieResult(
-#         imdb_code=md.get("imdb_code"),
-#         title=md.get("title"),
-#         duration=md.get("duration"),
-#         director=md.get("director"),
-#         year=md.get("year", 0),
-#         rating=md.get("rating", 0),
-#         imdb_score=md.get("imdb_score", 0.0),
-#         keywords=md.get("keywords"),
-#         genres=md.get("genres")
-#     )
-#     movies.append(m)
 
-# def method(x,y,z,**kwargs):
-#     pass
-
-
-# def method(7,2,z=2)
-
-
-movies = [
-    MovieResult(**md)
-    for md in movies_list
-]
-# for md in movies_list:
-#     m = MovieResult(**md)
-#     movies.append(m) 
+movies = [MovieResult(**md) for md in movies_list]
 
 print("Found {} movies from search {}".format(len(movies), search))
 
